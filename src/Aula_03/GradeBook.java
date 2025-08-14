d422f8562ecc8d61f1b134b5ab0ad2ff92f7d935
Gabriel Tome <gabriel.47824@alunosatc.edu.br>
2025-08-14 19:39:02 -0300

    update

---
+++ src/Aula_03/GradeBook.java
@@ -0,0 +1,7 @@
+package Aula_03;
+
+public class GradeBook {
+    public void displayMessage(){
+        System.out.println("Welcome to the GradeBook");
+    }
+}
---
+++ src/Aula_03/GradeBookTest.java
@@ -0,0 +1,8 @@
+package Aula_03;
+
+public class GradeBookTest {
+    public static void main(String[] args) {
+        GradeBook gradeBook = new GradeBook();
+        gradeBook.displayMessage();
+    }
+}
